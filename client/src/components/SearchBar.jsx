a7342ebe9be314338c714acb24e1aaa13b74964e
Arian Dostyar <dostyararian@gmail.com>
2025-07-30 16:09:32 -0700

    Removed dark mode features

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,9 +1,9 @@
 export default function SearchBar() {
   return (
-    <form className="w-full max-w-md flex-1 mx-auto ">
+    <form className="w-full max-w-md flex-1 mx-auto  ">
       <label
-        for="default-search"
-        className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+        htmlFor="default-search"
+        className="mb-2 text-sm font-medium text-gray-900 sr-only "
       >
         Search
       </label>
@@ -18,9 +18,9 @@ export default function SearchBar() {
           >
             <path
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
             />
           </svg>
@@ -28,13 +28,13 @@ export default function SearchBar() {
         <input
           type="search"
           id="default-search"
-          className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="block w-full p-4 ps-10 text-sm text-gray-900 border-2 border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-700"
           placeholder="Search by title"
           required
         />
         <button
           type="submit"
-          className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 "
         >
           Search
         </button>
