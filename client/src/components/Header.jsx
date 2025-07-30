09099990ceea8d09d2d32201195044ff68d27a3a
Arian Dostyar <dostyararian@gmail.com>
2025-07-30 02:08:47 -0700

    Fixed padding styling bug

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ import HeaderButton from "./HeaderButton";
 
 export default function Header() {
   return (
-    <div className="flex justify-between items-center bg-white p-4 shadow-md absolute top-0 left-0 right-0">
+    <div className="flex justify-between items-center bg-white p-4 shadow-md sticky top-0 ">
       <div className="flex flex-1 gap-2 items-baseline">
         <div className="flex max-h-max bg-black p-2 rounded-md ">
           <HeaderButton url="/" imgName="CourseText.svg" />
