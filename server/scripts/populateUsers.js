164113097ef4830882b8aeeba9c417fcaac45804
Arian Dostyar <dostyararian@gmail.com>
2025-07-30 19:59:28 -0700

    Dummy data script

--- server/scripts/populateUsers.js
+++ server/scripts/populateUsers.js
@@ -23,7 +23,14 @@ CREATE TABLE IF NOT EXISTS listings (
    price NUMERIC(7, 2),
    user_id INT,
    FOREIGN KEY (user_id) REFERENCES users(id)
-);`;
+);
+
+
+INSERT INTO listings (title, description, price, user_id) VALUES ('title 1', 'desc 1', 1.25, 1) ON CONFLICT DO NOTHING;
+INSERT INTO listings (title, description, price, user_id) VALUES ('title 2', 'desc 2', 2.25, 1) ON CONFLICT DO NOTHING;
+INSERT INTO listings (title, description, price, user_id) VALUES ('title 3', 'desc 3', 3.25, 1) ON CONFLICT DO NOTHING;
+
+`;
 
 async function main() {
   try {
