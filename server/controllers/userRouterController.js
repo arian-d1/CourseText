2fa21b0a08287216c8fc7690184e6d974a354a7d
Arian Dostyar <dostyararian@gmail.com>
2025-08-01 14:10:45 -0700

    Removed log

--- server/controllers/userRouterController.js
+++ server/controllers/userRouterController.js
@@ -34,8 +34,6 @@ async function getIdByUser(req, res) {
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-
-    console.log("Errors:", errors);
     const username = req.params.user;
     const response = await userModel.getIdByUser(username);
     res.json({ success: true, id: response });
