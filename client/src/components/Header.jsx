3b1243b493bbed41284f9b2d18d9144de5f93803
Arian Dostyar <dostyararian@gmail.com>
2025-08-05 21:16:56 -0700

    style

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="flex justify-between items-center bg-white p-4 shadow-lg sticky top-0 ">
       <div className="flex flex-1 gap-2 items-baseline">
-        <div className="flex max-h-max bg-black p-2 rounded-md ">
+        <div className="flex max-h-max bg-blue-800/90 p-2 rounded-md ">
           <HeaderButton url="/" imgName="CourseText.svg" />
         </div>
         <p className="text-3xl font-semibold tracking-wide text-gray-800">
