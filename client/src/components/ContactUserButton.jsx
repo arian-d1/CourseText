ed51eed4dab506c9d96a7d5071eca51da996c82b
Arian Dostyar <dostyararian@gmail.com>
2025-08-06 00:24:11 -0700

    require user to be logged in to contact a seller

--- client/src/components/ContactUserButton.jsx
+++ client/src/components/ContactUserButton.jsx
@@ -1,7 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import AuthContext from "../context/AuthProvider";
+
 export default function ContactUserButton({ setShowUser }) {
+    const { auth } = useContext(AuthContext);
+    const navigate = useNavigate(); 
+
   return (
     <button
-      onClick={() => setShowUser(true)}
+      onClick={() => {if (auth.state) {
+        setShowUser(true);
+      } else {
+        navigate("/log-in");
+      }}}
       className="p-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
     >
       Contact Seller
