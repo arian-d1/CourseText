52adbb42d668e1a758d5f7bc2d60461cfdb2e29e
Arian Dostyar <dostyararian@gmail.com>
2025-08-06 00:41:19 -0700

    Added names in sending feature

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
+import { getUserNameByID } from "../api/listings";
+
 export default function Message({ sender_id, receiver_id, message }) {
+  const [senderUsername, setSenderUsername] = useState(null);
+
+  useEffect(() => {
+    async function getUserName() {
+      try {
+        const response = await getUserNameByID(sender_id);
+        setSenderUsername(response.data.username);
+      } catch (error) {
+        throw new Error("Failed to fetch username: " + error.message);
+      }
+    }
+    getUserName();
+  }, []);
   return (
     <div className="bg-white border border-gray-200 p-4 rounded-md shadow-sm">
       <p className="text-gray-700">{message}</p>
-      <p className="text-xs text-gray-400 mt-2">From user ID: {sender_id}</p>
+      <p className="text-xs text-gray-400 mt-2">From {senderUsername}</p>
     </div>
   );
 }
