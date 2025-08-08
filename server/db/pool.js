442bc48f0a5fc82f36b8d3ce820f736e018ab14d
Arian Dostyar <dostyararian@gmail.com>
2025-08-07 20:53:36 -0700

    DATABASE_URL

--- server/db/pool.js
+++ server/db/pool.js
@@ -2,11 +2,10 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const db = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL, // Neon connection string
+  ssl: {
+    rejectUnauthorized: false, // required for Neon/Vercel Postgres
+  },
 });
 
 module.exports = db;
