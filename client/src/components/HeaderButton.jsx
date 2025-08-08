9551c031ad033abd7cc4bd199eaf17d553d73479
Arian Dostyar <dostyararian@gmail.com>
2025-08-08 01:31:02 -0700

    fixed image links

--- client/src/components/HeaderButton.jsx
+++ client/src/components/HeaderButton.jsx
@@ -1,5 +1,5 @@
 export default function HeaderButton({ url, imgName }) {
-  const imgSrc = "./src/public/" + imgName;
+  const imgSrc = "src/public/" + imgName;
   return (
     <a href={url} className=" hover:opacity-50 transition-opacity duration-300">
       <img src={imgSrc} alt={imgName} className="max-w-8"></img>
